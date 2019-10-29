4234bb0dc72a068e78ba4a6f9bc26f15cbd41583
Simon <meet@simon-frey.eu>
2019-10-29 17:02:39 +0100

    Add chrome extension

---
+++ webextension/chrome/background.js
@@ -0,0 +1,31 @@
+// Set unshort server
+var unshortPattern = "unshort.link";
+
+
+// Redirect services via unshort.link
+function redirect(requestDetails) {
+    return {
+        redirectUrl: "https://" + unshortPattern + "/d/" + requestDetails.url
+    };
+}
+
+// Load available services from server
+var req = new XMLHttpRequest();
+req.open("GET", "https://" + unshortPattern + "/providers", true);
+req.addEventListener("load", function () {
+    let servicesUrls = [];
+
+    let services = JSON.parse(req.response);
+    services.forEach(function (item, index) {
+        servicesUrls.push("*://" + item + "/*")
+    });
+
+
+    chrome.webRequest.onBeforeRequest.addListener(
+        redirect,
+        { urls: servicesUrls },
+        ["blocking"]
+    );
+});
+req.send(null);
+
